2f2f25d11d27402f3bad334433474d89d7b1db84
Antony Kahuro <kahuro91tony@gmail.com>
2023-07-09 15:09:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName ='bob';
+function upperCaseCustomerName(){
+    return customerName=customerName.toUpperCase();
+
+}
+
+function setBestCustomer(){
+    globalThis.bestCustomer='not bob';
+
+    return bestCustomer;
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer= "Jane";
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="Fred";
+
+    return leastFavoriteCustomer;
+}
